147bafbc4966f2426d0b53f48794336e8ac759ac
john-c025 <johntherencecalma@gmail.com>
2026-04-24 20:06:18 +0800

    added otp apis

--- lib/utils/api/endpoints/api_endpoints.dart
+++ lib/utils/api/endpoints/api_endpoints.dart
@@ -4,7 +4,7 @@ class ApiEndpoints {
 
   static const String baseUrl = 'https://ecom-api-staging.iam-ww.com/v1';
   static const String rootUrl = 'https://ecom-api-staging.iam-ww.com';
-
+  
   static const String authLogin = '/Auth/Login';
   static const String authSignup = '/Auth/Signup';
   static const String authResendVerificationCode = '/Auth/ResendVerificationCode';
